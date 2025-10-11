c9eca4c9dbee6810a82ddfac9138de7282b72a1a
Szczebrzeszyniec <vtrst98@gmail.com>
2025-10-11 19:07:25 +0200

    macos compatible installer

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@ chmod +x ./fssh.py
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     target="/usr/local/bin/fssh"
+    sudo mkdir -p /usr/local/bin
 else
     target="/usr/bin/fssh"
 fi
 
-cp -f ./fssh.py "$target"
-chmod 755 "$target"
+sudo cp -f ./fssh.py "$target"
+sudo chmod 755 "$target"
 
 echo
 echo "install your system package for python-paramiko and python-termios"
